f214f82bc45f5ebead0e02ccb8c9dc9c1786c88d
Dennis Schwertel <s@digitalkultur.net>
2012-05-02 17:36:26 +0200

    initial install script

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "not yet implemented"
+BACKUPDIR=~/.dotfiles/backup/
+#install vimrc
+
+
+if [[ ! -L "$HOME/.vimrc" ]]
+then
+  echo "installing vimrc, moving old to $BACKUPDIR"
+  mv $HOME/.vimrc $BACKUPDIR
+  ln -s $HOME/.dotfiles/dot/vimrc $HOME/.vimrc
+else
+  echo "vimrc already installed"
+fi
+
+
+echo "installing PS1"
+echo "if [[ -f \"\$HOME/.dotfiles/scripts/ps1\" ]]
+then
+  export PROMPT_COMMAND=\"source \$HOME/.dotfiles/scripts/ps1\"
+fi
+" >> $HOME/.bashrc
